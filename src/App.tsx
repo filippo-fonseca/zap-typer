108df60c30b1cb0b4f9f5a163267bcf4ce66c43b
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2024-07-06 11:39:10 -0600

    words

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { faker } from '@faker-js/faker';
+
+const words = faker.word.words(10);
 
 function App() {
-  return <div className='text-4xl text-center'>hghgh</div>;
+  return <div className='text-4xl text-center text-slate-500'>{words}</div>;
 }
 
 export default App;
